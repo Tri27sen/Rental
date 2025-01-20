9cfcafbca9848fe7cf45880d0c624210e11cdd1d
Tri27sen <trishasengupta27@gmail.com>
2025-01-20 20:37:09 +0530

    added the next-auth

--- src/app/libs/prismadb.ts
+++ src/app/libs/prismadb.ts
@@ -1,10 +1,16 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from '@prisma/client'
 
+// Use type augmentation for the global scope
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined
 }
 
-const client = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client
+// Create or reuse Prisma Client instance
+const client = globalThis.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = client
+}
 
 export default client
