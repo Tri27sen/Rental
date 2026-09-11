cfbb6c376ab9e02cc7877e1224ca3d32ea24ebee
Tri27sen <trishasengupta27@gmail.com>
2026-09-11 16:24:40 +0530

    removed github

--- src/app/components/modals/LoginModal.tsx
+++ src/app/components/modals/LoginModal.tsx
@@ -76,7 +76,7 @@ const LoginModal = () => {
     >
       <hr />
       <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => signIn("google")} />
-      <Button outline label="Continue with GitHub" icon={AiFillGithub} onClick={() => signIn("github")} />
+      
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="justify-center flex flex-row items-center gap-2">
           <div>First time using my-app?</div>
--- src/app/components/modals/RegisterModal.tsx
+++ src/app/components/modals/RegisterModal.tsx
@@ -101,12 +101,6 @@ const RegisterModal = () => {
         icon={FcGoogle}
         onClick={() => signIn("google")}
       />
-      <Button
-        outline
-        label="Continue with GitHub"
-        icon={AiFillGithub}
-        onClick={() => signIn("github")}
-      />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="justify-center flex flex-row items-center gap-2">
           <div>Already have an account?</div>
